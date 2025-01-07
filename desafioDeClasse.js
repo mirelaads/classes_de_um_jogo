6f61da7fa6620c75626c118d449cec4382dc6a99
mirelaads <mirelasouza1996@gmail.com>
2025-01-06 21:02:03 -0300

    desafio de classe Herói

--- desafioDeClasse.js
+++ desafioDeClasse.js
@@ -1,1 +1,24 @@
-console.log("Códgido");
+class heroiAsh {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+  atacar() {
+    let ataque = ""; //variavel para armazenar o tipo de ataque
+
+    if (this.tipo === "mago") {
+      ataque = "magia";
+    } else if (this.tipo === "guerreiro") {
+      ataque = "espada";
+    } else if (this.tipo === "monge") {
+      ataque = "usou artes marciais";
+    } else if (this.tipo === "ninja") {
+      ataque = "usou shuriken";
+    }
+    console.log(`O ${this.tipo} atacou usando ${ataque}`);
+  }
+}
+
+const heroi1 = new heroiAsh("Amy lee", 29, "mago");
+heroi1.atacar(); // saída
